eb9c569b2b6a4c9afea9fda76ab03c2957196152
Emily Pisaygnane <episaygnane@me.com>
2023-01-17 15:46:13 -0800

    imported tests and the corresponding file for each tests file

---
+++ src/components/ShoppingList/ShoppingList.test.jsx
@@ -0,0 +1,10 @@
+// 'shopping-list-name-test'
+// 'shopping-list--test'
+
+import {
+  fireEvent,
+  render,
+  screen,
+  TestingLibraryElementError,
+} from '@testing-library/react';
+import ShoppingList from './ShoppingList';
---
+++ src/components/ShoppingList/ShoppingListItemForm.test.jsx
@@ -0,0 +1,7 @@
+// import {
+//    fireEvent,
+//    render,
+//   screen,
+//    TestingLibraryElementError,
+//  } from '@testing-library/react';
+// import ShoppingListItemForm from './ShoppingListItemForm';
