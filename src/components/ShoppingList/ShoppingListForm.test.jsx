3084586d563941bfad9ad5cfd4defcd64bbc19b5
Emily Pisaygnane <episaygnane@me.com>
2023-01-17 15:19:49 -0800

    created a test to submit the form when the button gets clicked, test passes

---
+++ src/components/ShoppingList/ShoppingListForm.test.jsx
@@ -0,0 +1,36 @@
+import { 
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react';
+import ShoppingListForm from './ShoppingListForm';
+
+describe('ShoppingListForm', () => {
+
+  it ('submits the form with button click', () => {
+    const onSubmit = jest.fn();
+
+    const defaultShoppingList = {
+      id: null,
+      name: 'shopping list',
+      shoppingItems: [],
+    };
+
+    render(
+      <ShoppingListForm id="test" onSubmit={onSubmit} />
+    );
+
+    const input = screen.getByTestId('shopping-list-form-name-test');
+    fireEvent.change(input, { target: { value: 'shopping list'} });
+
+    const submitButton = screen.getByTestId(
+      'shopping-list-form-submit-button-test'
+    );
+    fireEvent.click(submitButton);
+
+    expect(onSubmit).toHaveBeenCalledWith(defaultShoppingList);
+  });
+
+  
+  })
+});
