3719d5e3038693cc4a60d986de545a60bc2cad48
Emily Pisaygnane <episaygnane@me.com>
2023-01-27 10:25:19 -0800

    wrote out shopping list item component

--- src/components/ShoppingList/ShoppingListItem.jsx
+++ src/components/ShoppingList/ShoppingListItem.jsx
@@ -1,5 +1,56 @@
-export default function ShoppingListItem() {
-  return <>
-    <div data-testid="shopping-list-item"></div>
-  </>;
-}
+import React from 'react';
+
+const ShoppingListItem = ({
+  item,
+  onUpdateItem,
+  onDeleteItem,
+}) => {
+  const [updatedItem, setUpdatedItem] = React.useState({
+    ...item,
+  });
+
+  const handleChange = (e) => {
+    setUpdatedItem({
+      ...updatedItem,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          name="item_name"
+          value={updatedItem.item_name}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="quantity"
+          value={updatedItem.quantity}
+          onChange={handleChange}
+        />
+      </form>
+      <span>
+        {item.item_name}: {item.quantity}
+      </span>
+      <br />
+      <button
+        data-testid={'update-shopping-item-$item.id}'}
+        onClick={() => onUpdateItem(updatedItem)}
+      >
+        Update
+      </button>
+      <br />
+      <button
+        data-testid={`delete-shopping-item-${item.id}`}
+        onClick={() => onDeleteItem(item)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default ShoppingListItem;
